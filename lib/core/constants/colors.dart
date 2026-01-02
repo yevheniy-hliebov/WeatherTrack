a784c751838e45ff12c9589165de372a3044090a
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2026-01-02 21:06:53 +0200

    feat: add AppColors class for theming and color constants

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  AppColors._();
+
+  static const Color primary = Color(0xFFF4F4F4);
+  static const Color onPrimary = Color(0xFF242424);
+  static const Color scaffoldBackground = Color(0xB3FFFFFF);
+
+  static const Color text = Color(0xFFF4F4F4);
+  static const Color inverseText = Color(0xFF242424);
+
+  static const Color weatherIconBg = Color(0xFF2B2B2B);
+  static const Color divider = Color(0x99F4F4F4);
+  static const Color transparent = Colors.transparent;
+  
+  static const Color splashColor = Color(0x59F4F4F4); 
+
+  static const Color glassBg = Color(0x735C5C5C);
+  
+  static const LinearGradient glassGradient = LinearGradient(
+    colors: [
+      Color(0x66FFFFFF),
+      Color(0x26FFFFFF),
+    ],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+}
