44a84485c986327328b30f52e349483fee93134d
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2026-01-03 11:16:28 +0200

    feat: add Spacing class for consistent spacing constants

---
+++ lib/core/constants/spacing.dart
@@ -0,0 +1,10 @@
+abstract class Spacing {
+  const Spacing._();
+
+  static const double xs = 5;
+  static const double sm = 10;
+  static const double md = 15;
+  static const double lg = 20;
+  static const double xl = 25;
+  static const double xxl = 30;
+}
