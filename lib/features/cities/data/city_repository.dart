2a00c320ddbd234db1bdf81a26f6108e4d0179d6
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2026-01-04 09:44:50 +0200

    feat: implement CityRepository for searching cities with error handling

---
+++ lib/features/cities/data/city_repository.dart
@@ -0,0 +1,57 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:weathertrack/core/constants/api.dart';
+import 'package:weathertrack/features/cities/data/city_exceptions.dart';
+import 'package:weathertrack/features/cities/data/city_model.dart';
+
+class CityRepository {
+  final http.Client client;
+
+  CityRepository({http.Client? client}) : client = client ?? http.Client();
+
+  Future<List<CityModel>> search(String namePrefix) async {
+    final uri = Uri.parse('${Api.geo.url}/cities').replace(
+      queryParameters: {
+        'minPopulation': Api.geo.population.toString(),
+        'namePrefix': namePrefix,
+      },
+    );
+
+    try {
+      final response = await http.get(
+        uri,
+        headers: {'X-RapidAPI-Key': Api.geo.xRapidapiKey},
+      );
+
+      if (response.statusCode == 200) {
+        String decodedString = utf8.decode(response.bodyBytes);
+        final decodedResponse = jsonDecode(decodedString);
+        return CityModel.fromJsonList(decodedResponse['data']);
+      } else {
+        throw CitySearchException(
+          type: CityExceptionType.server,
+          message: 'Server Error: ${response.statusCode}',
+        );
+      }
+    } on SocketException {
+      throw CitySearchException(
+        type: CityExceptionType.network,
+        message: 'No internet connection',
+      );
+    } on TimeoutException {
+      throw CitySearchException(
+        type: CityExceptionType.network,
+        message: 'Connection timed out',
+      );
+    } on CitySearchException {
+      rethrow;
+    } catch (e) {
+      throw CitySearchException(
+        type: CityExceptionType.unknown,
+        message: e.toString(),
+      );
+    }
+  }
+}
