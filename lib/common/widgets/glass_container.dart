6e0149b4e99ed3065629cccd7dcbc2691636ab6a
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2026-01-06 23:47:43 +0200

    feat: add RoundedRadius class for defining consistent radius values

--- lib/common/widgets/glass_container.dart
+++ lib/common/widgets/glass_container.dart
@@ -2,14 +2,15 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:weathertrack/core/constants/colors.dart';
+import 'package:weathertrack/core/constants/rounded_radius.dart';
 
 class GlassContainer extends StatelessWidget {
   final bool bordered;
   final bool transparency;
   final EdgeInsets? padding;
+  final double radius;
   final Widget? child;
 
-  final double radius = 25;
   final double blurValue = 4;
 
   const GlassContainer({
@@ -17,6 +18,7 @@ class GlassContainer extends StatelessWidget {
     this.bordered = false,
     this.transparency = true,
     this.padding,
+    this.radius = RoundedRadius.lg,
     this.child,
   });
 
---
+++ lib/core/constants/rounded_radius.dart
@@ -0,0 +1,9 @@
+abstract class RoundedRadius {
+  const RoundedRadius._();
+
+  static const double xs = 5;
+  static const double sm = 10;
+  static const double md = 15;
+  static const double lg = 20;
+  static const double xl = 25;
+}
