ec47d78fb7013bfd77a03e7ed89b6a5f427334b7
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2026-01-12 12:46:23 +0200

    refactor(city-model): remove unused fields from CityMode class

--- lib/common/models/city_model.dart
+++ lib/common/models/city_model.dart
@@ -1,30 +1,16 @@
 class CityModel {
   final int id;
-  final String wikiDataId;
-  final String type;
-  final String city;
   final String name;
-  final String country;
   final String countryCode;
-  final String region;
-  final String regionCode;
   final double latitude;
   final double longitude;
-  final double population;
 
   const CityModel({
     required this.id,
-    required this.wikiDataId,
-    required this.type,
-    required this.city,
     required this.name,
-    required this.country,
     required this.countryCode,
-    required this.region,
-    required this.regionCode,
     required this.latitude,
     required this.longitude,
-    required this.population,
   });
 
   String get nameAndCountryCode => '$name, ${countryCode.toUpperCase()}';
@@ -32,17 +18,10 @@ class CityModel {
   factory CityModel.fromJson(Map<String, dynamic> json) {
     return CityModel(
       id: int.parse(json['id'].toString()),
-      wikiDataId: json['wikiDataId'],
-      type: json['type'],
-      city: json['city'],
       name: json['name'],
-      country: json['country'],
       countryCode: json['countryCode'],
-      region: json['region'],
-      regionCode: json['regionCode'],
       latitude: double.parse(json['latitude'].toString()),
       longitude: double.parse(json['longitude'].toString()),
-      population: double.parse(json['population'].toString()),
     );
   }
 
@@ -53,17 +32,10 @@ class CityModel {
   Map<String, dynamic> toJson() {
     return {
       'id': id,
-      'wikiDataId': wikiDataId,
-      'type': type,
-      'city': city,
       'name': name,
-      'country': country,
       'countryCode': countryCode,
-      'region': region,
-      'regionCode': regionCode,
       'latitude': latitude,
       'longitude': longitude,
-      'population': population,
     };
   }
 }
--- test/fixtures/city_fixtures.dart
+++ test/fixtures/city_fixtures.dart
@@ -5,31 +5,17 @@ class CityFixtures {
     'data': <dynamic>[
       {
         'id': 3350606,
-        'wikiDataId': 'Q24668',
-        'type': 'CITY',
-        'city': 'Aixirivall',
         'name': 'Aixirivall',
-        'country': 'Andorra',
         'countryCode': 'AD',
-        'region': 'Sant Julià de Lòria',
-        'regionCode': '06',
         'latitude': 42.46245,
         'longitude': 1.50209,
-        'population': 0,
       },
       {
         'id': 3216144,
-        'wikiDataId': 'Q24656',
-        'type': 'CITY',
-        'city': 'Aixovall',
         'name': 'Aixovall',
-        'country': 'Andorra',
         'countryCode': 'AD',
-        'region': 'Sant Julià de Lòria',
-        'regionCode': '06',
         'latitude': 42.47635833,
         'longitude': 1.48949167,
-        'population': 0,
       },
     ],
   };
@@ -41,17 +27,10 @@ class CityFixtures {
   static final List<CityModel> mockHistory = List.generate(5, (i) {
     return CityModel(
       id: i + 10,
-      wikiDataId: 'Q${i + 11}',
-      type: 'CITY',
-      city: 'City $i',
       name: 'City $i',
-      country: 'FakeCountry',
       countryCode: 'FC',
-      region: 'City $i region',
-      regionCode: '$i',
       latitude: i * 1.1,
       longitude: i * 2.2,
-      population: 1 * 5000,
     );
   });
 }
